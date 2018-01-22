b35847c1c8609651e6d9e75b28fbf7d9887020c3
Anton Lindblad <anton.j.lindblad@gmail.com>
2018-01-22 18:37:00 +0100

    update

--- main.js
+++ main.js
@@ -3,11 +3,8 @@
 function HelloWorld(){
     console.log("Hello World");
     console.log("sam was here!");
-<<<<<<< HEAD
     console.log("Anton too!");
-=======
     console.log("he uploaded work");
->>>>>>> 2783318d903713560f8af96baf89fd15729fba45
 }
 
 HelloWorld();
