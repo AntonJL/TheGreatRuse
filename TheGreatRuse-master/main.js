0d67a4f0c75ad6624318988249e2f0317aceadd2
Anton Lindblad <anton.j.lindblad@gmail.com>
2018-01-22 18:37:24 +0100

    removed folder

--- TheGreatRuse-master/main.js
+++
@@ -1,9 +0,0 @@
-// COLLABORATION BY SAMUEL HAWKINS AND ANTON LINDLBAD
-
-function HelloWorld(){
-    console.log("Hello World");
-    console.log("sam was here!");
-    console.log("he uploaded work");
-}
-
-HelloWorld();
